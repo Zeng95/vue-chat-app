18cf26da26b41df905911bfb7d3568dd5607d0b1
Zeng Tao <qingchun1995@gmail.com>
2020-05-05 15:01:11 +0800

    first commit

---
+++ src/views/Login.vue
@@ -0,0 +1,45 @@
+<template>
+  <div class="login">
+    <NavBar />
+
+    <b-container>
+      <b-row>
+        <b-col lg="3"></b-col>
+        <b-col lg="6">
+          <b-card title="Login" title-tag="h2" align="center">
+            <b-form @submit="onSubmit">
+              <b-form-group>
+                <b-form-input
+                  required
+                  v-model="form.username"
+                  placeholder="Please enter your name ..."
+                ></b-form-input>
+              </b-form-group>
+
+              <b-button type="submit" variant="primary">Enter Chat</b-button>
+            </b-form>
+          </b-card>
+        </b-col>
+        <b-col lg="3"></b-col>
+      </b-row>
+    </b-container>
+  </div>
+</template>
+
+<script>
+import NavBar from '@/components/NavBar'
+
+export default {
+  components: { NavBar },
+  data() {
+    return {
+      form: {
+        username: ''
+      }
+    }
+  },
+  methods: {
+    onSubmit() {}
+  }
+}
+</script>
